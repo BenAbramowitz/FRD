2054f6d0c78b59623445a43b663a81e8795cc760
Ben Abramowitz <benjabramowitz@gmail.com>
2023-07-07 14:48:27 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,22 +11,22 @@ NOTE: Currently, you want to vary only one independent variable per experiment i
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    N_ITER = 100
+    N_ITER = 1000
     SEED = None
 
-    VERBOSE = False #whether to print info while running simulation
+
     SAVE = True #whether to save experiment data automatically before returning
     MOMENTS = True #whether to analyze the sample moments of each experiment after data is saved
 
-    N_VOTERS = [31]
-    N_CANDS = [10]
+    N_VOTERS = [11]
+    N_CANDS = [60]
     N_ISSUES = [20,40,60,80,100]
-    VOTERS_P = [0.55] #Bernoulli p for generating voter issue prefs
-    CANDS_P = [0.55] #Bernoulli p for generating cand issue prefs
+    VOTERS_P = [0.75] #Bernoulli p for generating voter issue prefs
+    CANDS_P = [0.25] #Bernoulli p for generating cand issue prefs
     APPROVAL_K = [N_CANDS[-1]]
     APPROVAL_THRESH = [0.5]
     ELECTION_RULES = ['max_approval','borda', 'plurality', 'rav', 'max_agreement', 'random_winners']
-    N_REPS = [3]
+    N_REPS = [11]
     DEFAULT_STYLE = ['uniform']
     DEFAULT_PARAMS = [None]
     DELEGATION_STYLE = [None]
@@ -52,23 +52,20 @@ if __name__ == '__main__':
                         'delegation_style':DELEGATION_STYLE,
                         'delegation_params':DELEGATION_PARAMS}
     
+
+    ##########################################
     np.random.seed(SEED)
-    data, param_names, n_iter, experiment_params, filename = simulate.run_simulation(N_ITER, profile_param_vals, 
-                                                      election_param_vals, 
-                                                      del_voting_param_vals, 
-                                                      verbose=VERBOSE,
-                                                      save=SAVE)
+    data, param_names, n_iter, experiment_params, filename = simulate.sim_parallel(N_ITER, profile_param_vals, election_param_vals, del_voting_param_vals, save=SAVE)
+    if SAVE:
+        data_loaded = save_data.unpickle_data(filename)
+        print(f'Experiment data saved to: {filename}')
+        if MOMENTS: 
+            analysis.get_moments(filename, param_names, save=True)
+            print('Moments computed and saved')
     print('-----------------------------')
     end = time.perf_counter()
     print(f'Runtime: {end-start}')
     print(f'Runtime per iter: {(end-start)/N_ITER}')
-    if SAVE: 
-        print(f'Experiment data saved to: {filename}')
-        data_loaded = save_data.unpickle_data(filename)
-        #print(data_loaded)
-        if MOMENTS: 
-            analysis.get_moments(filename, param_names, save=True)
-            print('Moments computed and saved')
     print('-----------------------------')
 
 
