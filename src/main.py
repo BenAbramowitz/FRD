727022a268a8eedbac0a682c752cc131c6ff531e
Ben Abramowitz <benjabramowitz@gmail.com>
2023-07-11 12:51:42 -0500

    Cleanup

--- src/main.py
+++ src/main.py
@@ -8,7 +8,9 @@ import frd.m05_simulate as simulate
 import frd.m06_analysis as analysis
 
 '''
-NOTE: Currently, you want to vary only one independent variable per experiment if multiple rules are being compared. This makes life easier when it comes to plotting. More generally, you want to vary only one variable to place on the x-axis of the plot, and optionally one more variable (e.g. election rules) to plot multiple lines on a line plot.
+NOTE: Currently, you want to vary only one independent variable per experiment if multiple rules are being compared. This makes life easier when it comes to plotting. More generally, you want to vary only one variable to place on the x-axis of the plot, and optionally one more variable (e.g. election rules) to plot multiple lines on that line plot.
+
+election rules: ["max_approval","borda","plurality","rav", "max_agreement", "random_winners"]
 '''
 
 if __name__ == '__main__':
@@ -19,6 +21,7 @@ if __name__ == '__main__':
 
     for e in experiments.keys(): #run all experiments
         print('-----------------------------')
+        print('Experiment ', e)
         experiment_params = experiments[e]
         n_iter = experiment_params["n_iter"]
         save = experiment_params["save"]
@@ -34,10 +37,10 @@ if __name__ == '__main__':
         np.random.seed(seed)
         data, param_names, n_iter, experiment_params, filename = simulate.sim_parallel(n_iter, profile_param_vals, election_param_vals, del_voting_param_vals, save=save)
         if save:
+            print(f'Experiment data saved to: {filename} using pickle')
             data_loaded = save_data.unpickle_data(filename)
-            print(f'Experiment data saved to: {filename}')
             analysis.get_moments(filename, param_names, save=True)
-            print('Moments computed and saved')
+            print('Moments computed and saved as csv')
         
         end = time.perf_counter()
         print(f'Runtime: {end-start}')
