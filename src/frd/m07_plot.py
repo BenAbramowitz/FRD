a7da5e82dac082c733001a212f02a1ee1368c9fc
Ben Abramowitz <benjabramowitz@gmail.com>
2023-07-12 13:13:10 -0500

    Added compare_all to plot all data in data dir and save plots
    Handles experiments with 1 or 2 independent variables only.
    Also improved formatting of plots.

--- src/frd/m07_plot.py
+++ src/frd/m07_plot.py
@@ -18,11 +18,14 @@ def check_filetype(filename, filetype='csv'):
         raise ValueError(f'File {filename} is not of correct type. Need {filetype}')
     
 def var_to_title(s):
+    if type(s) is not str:
+        return s
     s = s.replace('_', ' ')
-    return s.title()
+    if s.lower() in ['rav', 'irv']: return s.upper() #handle known acronyms
+    else: return s.title()
 
 
-def label_compare_rules_plot(x_var, y_var):
+def label_plot(x_var, y_var):
     '''
     
     TO DO
@@ -35,7 +38,7 @@ def label_compare_rules_plot(x_var, y_var):
     # datafile = str([f for f in listdir(path) if isfile(join(path, f)) and f[0:3] == prefix and 'RD' in f and 'moments' not in f])
 
     title = ""
-    # x_label, y_label = "", ""
+    x_label, y_label = "", ""
     # if "FRD" in datafile:
     #     title = "FRD: "
     # elif "RD" in datafile:
@@ -121,7 +124,7 @@ def compare_rules(filename, experiment_name, x_var:str, y_var='mean', save=True,
     df['election_rules'] = df['election_rules'].apply(lambda x: var_to_title(x))
 
     p = sns.lineplot(data=df, x=x_var, y=y_var, hue='election_rules')
-    title, xlabel, ylabel = label_compare_rules_plot(x_var, y_var)
+    title, xlabel, ylabel = label_plot(x_var, y_var)
     if y_var == 'mean': p.set_yticks(np.arange(0,101,10)/100)
     p.legend(title='Election Rules')
 
@@ -131,3 +134,70 @@ def compare_rules(filename, experiment_name, x_var:str, y_var='mean', save=True,
         fig.savefig('./plots/'+experiment_name+'_compare_rules_'+y_var+'_vs_'+x_var)
     if show:
         plt.show()
+
+
+def plot_one_var(filename, experiment_name, x_var:str, y_var='mean', save=True, show=False, data_dir='./data/'):
+    check_filetype(filename, 'csv')
+    df = pd.read_csv(data_dir+filename)
+    df[x_var] = df[x_var].apply(lambda x: var_to_title(x))
+    p = sns.lineplot(data=df, x=x_var, y=y_var)
+    title, xlabel, ylabel = label_plot(x_var, y_var)
+    if y_var == 'mean': p.set_yticks(np.arange(0,101,10)/100)
+    p.set(title=title, xlabel=xlabel, ylabel=ylabel)
+    if save:
+        fig = p.get_figure()
+        fig.savefig('./plots/'+experiment_name+'_'+y_var+'_vs_'+x_var)
+    if show:
+        plt.show()
+
+def compare(filename, experiment_name, l_var:str, x_var:str, y_var='mean', save=True, show=False, data_dir='./data/'):
+    check_filetype(filename, 'csv')
+    df = pd.read_csv(data_dir+filename)
+    df[l_var] = df[l_var].apply(lambda x: var_to_title(x))
+    df[x_var] = df[x_var].apply(lambda x: var_to_title(x))
+
+    p = sns.lineplot(data=df, x=x_var, y=y_var, hue=l_var)
+    title, xlabel, ylabel = label_plot(x_var, y_var)
+    if y_var == 'mean': p.set_yticks(np.arange(0,101,10)/100)
+    p.legend(title=var_to_title(l_var))
+    if x_var.lower() == 'election_rules':
+        p.set_xticks(p.get_xticks()) #dumb hack to prevent set_xticklabels from issuing a warning
+        p.set_xticklabels(p.get_xticklabels(), rotation=20, ha="right")
+    p.set(title=title, xlabel=xlabel, ylabel=ylabel)
+    if save:
+        fig = p.get_figure()
+        fig.savefig('./plots/'+experiment_name+'_compare_'+l_var+'_'+y_var+'_vs_'+x_var)
+        plt.close(fig)
+    if show:
+        plt.show()
+    
+
+def compare_all(data_dir='./data/', y_var='mean', save=True, show=True)->None:
+    path = Path("./data")
+    momentfiles = sorted([f for f in listdir(path) if isfile(join(path, f)) and '_moments' in f])
+    datafiles = sorted([f for f in listdir(path) if isfile(join(path, f)) and '_data' in f])
+    for idx,f in enumerate(momentfiles):
+        check_filetype(f, 'csv')
+        experiment_name = f[0:-12]
+        if experiment_name != datafiles[idx][0:-5]:
+            print(f'Mismatch between data and moments files in {data_dir}: {f}')
+            print(datafiles[idx][0:-5])
+            continue
+        df = pd.read_csv(data_dir+f)
+        varied = get_columns_with_multiple_unique_values(df.iloc[:,1:14])
+        if len(varied) > 2:
+            print(f'Moments file contains more than two independent variables, cannot automatically plot comparisons: {f}')
+            continue
+        elif len(varied) == 2:
+            compare(f, experiment_name, l_var=varied[0], x_var=varied[1], y_var=y_var, save=save, show=show, data_dir=data_dir)
+            compare(f, experiment_name, l_var=varied[1], x_var=varied[0], y_var=y_var, save=save, show=show, data_dir=data_dir)
+        elif len(varied) == 1:
+            plot_one_var(f, experiment_name, x_var=varied[0], y_var=y_var, save=save, show=show, data_dir=data_dir)
+
+def get_columns_with_multiple_unique_values(df)->list:
+    columns_with_multiple_unique_values = []
+    for column in df.columns:
+        unique_values = df[column].nunique()
+        if unique_values > 1:
+            columns_with_multiple_unique_values.append(str(column))
+    return columns_with_multiple_unique_values
