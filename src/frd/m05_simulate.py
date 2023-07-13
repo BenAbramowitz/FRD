8e3bd35987407878c4596c3640de60cc0a5b5ccc
Ben Abramowitz <benjabramowitz@gmail.com>
2023-07-13 02:24:02 -0500

    Fixed bugs and
    Made it so that election is not rerun if profile and election params have not changed by reusing an RD or FRD object instead of creating a new one each time.

--- src/frd/m05_simulate.py
+++ src/frd/m05_simulate.py
@@ -39,16 +39,28 @@ def single_iter(profile_param_vals:tuple, election_param_vals:dict, del_voting_p
         for election_params in helper.params_dict_to_tuples(election_param_vals)[0]:
             # elect reps to get rep_ids and election_scores (if election rule provides scores)
             election_rule_name, n_reps = election_params
-            if n_reps > n_cands: break #skip nonsenical case where number of reps to elect is greater than number of cands
+            if n_reps > n_cands: continue #skip nonsenical case where number of reps to elect is greater than number of cands
+
+            #create rd and frd objects to be reused where necessary, depending on delegation params. Run elections only once per iter
+            made_rd=False
+            if None in del_voting_param_vals['delegation_style']:
+                rd = d_voting.RD(prof, election_rule_name, n_reps, default='uniform')
+                rd.elect_reps()
+                rd.pull_rep_prefs()
+                made_rd=True
+            if del_voting_param_vals['delegation_style'] != [None]:
+                frd = d_voting.FRD(prof, election_rule_name, n_reps, del_style=None, best_k=None, n_delegators = None, default='uniform')
+                if made_rd == True: frd.set_rep_ids(rd.get_rep_ids()) #avoid running same election twice, use same reps from rd object
+                else: frd.elect_reps()
             
             for del_voting_params in helper.params_dict_to_tuples(del_voting_param_vals)[0]:
                 default, del_style, best_k, n_delegators = del_voting_params
                 if del_style is None: #RD
-                    rd = d_voting.RD(prof, election_rule_name, n_reps, default=default)
-                    agreement = rd.run_RD()
+                    rd.set_default(default)
+                    agreement = rd.run_RD(quick=True)
                 else: #FRD
-                    frd = d_voting.FRD(prof, election_rule_name, n_reps, del_style=del_style, best_k=best_k, n_delegators = n_delegators, default=default)
-                    agreement = frd.run_FRD()
+                    frd.set_delegation_params(default=default, del_style=del_style, best_k=best_k, n_delegators = n_delegators)
+                    agreement = frd.run_FRD(quick=True)
                 data[tuple_to_hashable(profile_params+election_params+del_voting_params)] = [agreement]
     return data
 
@@ -57,6 +69,7 @@ def single_iter_unpacker(args):
 
 def sim_parallel(n_iter:int, profile_param_vals:dict, election_param_vals:dict, del_voting_param_vals:dict, save:bool=True, experiment_name=None):
     data = {}
+    print(f'Parallelizing iterations on up to {mp.cpu_count()-1} CPUs')
     with Pool(mp.cpu_count()-1) as pool:
         for iter_data in pool.imap_unordered(single_iter_unpacker, [[profile_param_vals, election_param_vals, del_voting_param_vals]]*n_iter):
             helper.append_dict_values(data, iter_data)
