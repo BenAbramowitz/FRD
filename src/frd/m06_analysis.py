d26524ba8ec19fb2040eaf8d8ca2b5e69e99540a
Ben Abramowitz <benjabramowitz@gmail.com>
2023-07-13 11:59:08 -0500

    Added option to save data to different directory (e.g. for test data)

--- src/frd/m06_analysis.py
+++ src/frd/m06_analysis.py
@@ -18,7 +18,7 @@ def four_moments(array):
     kurtosis = stats.kurtosis(array)
     return [mean, variance, skew, kurtosis]
 
-def get_moments(filename, param_names, save=True):
+def get_moments(filename, param_names, save=True, data_dir='./data'):
     '''
     Load data from file, compute moments for each parameterization in that experiment, then save analysis as csv
 
@@ -26,12 +26,12 @@ def get_moments(filename, param_names, save=True):
     -------
     df (pd.DataFrame): has col for each parameter val and columns for mean, variance, skew, and kurtosis of the agreements data, row for each parameterization
     '''
-    data = save_data.unpickle_data(filename)
-    path = Path("./data")
+    data = save_data.unpickle_data(filename, data_dir=data_dir)
+    path = Path(data_dir)
 
     analyzed = [list(params) + four_moments(agreements) for params, agreements in data.items()]
     df = pd.DataFrame(analyzed, columns = param_names+['mean','variance','skew','kurtosis'])
     if save == True: 
         filename = filename.partition('_data')[0]+'_moments.csv'
         df.to_csv(os.path.join(path, filename))
-    return df
+    return df, filename
