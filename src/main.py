61d954989eea9429205111ed89846b7f3a1aed91
Ben Abramowitz <benjabramowitz@gmail.com>
2023-07-13 11:59:58 -0500

    Added option to save data to different directory (e.g. for test data)

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ import frd.m07_plot as plot
 NOTE: Currently, you want to vary only one independent variable per experiment if multiple rules are being compared. This makes life easier when it comes to plotting. More generally, you want to vary only one variable to place on the x-axis of the plot, and optionally one more variable (e.g. election rules) to plot multiple lines on that line plot.
 '''
 
-EXPERIMENTS = ['RD_cands_p','RD_cands_p_biased','1a', '1b', '1c', '1a_polarized', '1a_similar', '1b_polarized', '1b_similar', '1c_polarized', '1c_similar'] #which experiments to run
+EXPERIMENTS = ['RD_reps','RD_cands'] #Set which experiments to run. Runs all if list is empty
+N_ITER = 1
 
 if __name__ == '__main__':
     p = Path(__file__).with_name('config.json')
@@ -21,14 +22,17 @@ if __name__ == '__main__':
         f.close()
 
     save=True
+    show=False
     timer=True
-    n_iter = 1
+    data_dir = './data/'
+    
     np.random.seed(1)
 
     for experiment_name in experiments.keys():
-        if experiment_name not in EXPERIMENTS: continue #which experiments to run
+        if EXPERIMENTS and experiment_name not in EXPERIMENTS: continue #which experiments to run
         print('-----------------------------')
         print('Experiment', experiment_name)
+        print(f'Running {N_ITER} iterations')
         #unpack experiment parameters
         experiment_params = experiments[experiment_name]
         profile_param_vals = experiment_params["profile_param_vals"]
@@ -38,14 +42,16 @@ if __name__ == '__main__':
         if timer: start = time.perf_counter()
 
         #run simulation for this experiment
-        data, param_names, n_iter, experiment_params, filename = simulate.sim_parallel(n_iter, profile_param_vals, election_param_vals, del_voting_param_vals, save=save, experiment_name = experiment_name)
+        data, param_names, n_iter, experiment_params, filename = simulate.sim_parallel(N_ITER, 
+        profile_param_vals, election_param_vals, del_voting_param_vals, save=save, experiment_name = experiment_name,data_dir=data_dir)
 
-        #save data and anlysis (i.e. moments)
-        if save:
+        #save data and analysis (i.e. moments) and generate plots
+        if save == True:
             print(f'Experiment data saved to: {filename} using pickle')
-            data_loaded = save_data.unpickle_data(filename)
-            analysis.get_moments(filename, param_names, save=True)
+            _, momentsfile = analysis.get_moments(filename, param_names, save=True, data_dir=data_dir)
             print('Moments computed and saved as csv')
+            plot.plot_moments(momentsfile, y_var='mean', save=True, show=show, data_dir=data_dir) #will generate one plot for each independent variable, up to two
+            print('Plots created for mean agreement')
         
         #Report runtime
         if timer:
@@ -54,6 +60,6 @@ if __name__ == '__main__':
             print(f'Avg runtime per iteration: {(end-start)/n_iter}')
 
     #Create plots for all data in the data folder and save/show them
-    if save: plot.compare_all(data_dir='./data/', y_var='mean', save=True, show=False)
+    # if save: plot.compare_all(data_dir='./data/', y_var='mean', save=True, show=False)
 
 
